67b28249ed6f75c265a14defb8869605d72246a7
Jonah Ryan <jopryan@ucsc.edu>
2023-04-21 14:50:59 -0700

    Added score and explosions!

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,12 +7,16 @@ class Play extends Phaser.Scene{
         // Load images
         this.load.image('rocket','./assets/rocket.png');
         this.load.image('spaceship','./assets/spaceship.png');
-        this.load.image('starfield','./assets/starfield.png')
+        this.load.image('starfield','./assets/starfield.png');
+        // load spritesheet
+        this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
     }
 
     create(){
-        this.starfield = this.add.tileSprite(0,0,640,480,'starfield').setOrigin(0,0);
+        this.p1Score = 0;
 
+        this.starfield = this.add.tileSprite(0,0,640,480,'starfield').setOrigin(0,0);
+        
         // green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
         // white borders
@@ -23,7 +27,7 @@ class Play extends Phaser.Scene{
     
         // add Rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width / 2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5,0);
-    
+
         // add spaceships (x3)
         this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
         this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
@@ -34,13 +38,76 @@ class Play extends Phaser.Scene{
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
+        this.anims.create({
+            key: 'explode',
+            frames: this.anims.generateFrameNumbers('explosion', {start: 0, end: 9, first: 0}),
+            frameRate: 30
+        });
+
+           // display score
+           let scoreConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+            top: 5,
+            bottom: 5,
+            },
+            fixedWidth: 100
+        }
+        this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
     }
 
     update(){
+    
         this.starfield.tilePositionX -= 4;
         this.p1Rocket.update();
         this.ship01.update();               // update spaceships (x3)
         this.ship02.update();
         this.ship03.update();
+        
+        // Check Collisions
+        if(this.checkCollision(this.p1Rocket,this.ship01)){
+            this.p1Rocket.reset();
+            this.shipExplode(this.ship01);   
+        }
+        if(this.checkCollision(this.p1Rocket,this.ship02)){
+            this.p1Rocket.reset();
+            this.shipExplode(this.ship02);   
+        }
+        if(this.checkCollision(this.p1Rocket,this.ship03)){
+            this.p1Rocket.reset();
+            this.shipExplode(this.ship03);   
+        }
+    }
+
+    checkCollision(rocket, ship){
+        // AABB checking
+        if(rocket.x < ship.x + ship.width &&
+            rocket.x + rocket.width > ship.x &&
+            rocket.y < ship.y + ship.height &&
+            rocket.height + rocket.y > ship.y){
+                return true;
+            }else{
+                return false;
+            }
+    }
+
+    shipExplode(ship){
+        ship.alpha = 0;
+        
+        let boom = this.add.sprite(ship.x,ship.y, 'explosion').setOrigin(0,0);
+        boom.anims.play('explode');
+        boom.on('animationcomplete', () => {
+            ship.reset();
+            ship.alpha = 1;
+            boom.destroy();
+        });
+
+        this.p1Score += ship.points;
+        this.scoreLeft.text = this.p1Score;
     }
 }
