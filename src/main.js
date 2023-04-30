8aac01bd289163dcf86f609f1a7b0ffba9f2e8e3
Jonah Ryan <jopryan@ucsc.edu>
2023-04-30 12:00:49 -0700

    Added Credit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
-'use strict';
+/*
 
+Author: Jonah Ryan
+Title:  Also Rocket Patrol
+Time:   13 hours
+Mods:   (New Enemy 15) (Two Player Mode 15) (Timer increases on hit 15)
+
+        (Parallax Background 10) (New Title screen 10) (Texture atlas enemy animations 10)
+        (time remaining displayed 10) (4 random explosion sfx 10)
+
+        (New scrolling Tile sprite for background 5)
+
+        (15 + 15 + 15) + (10 + 10 + 10 + 10 + 10) + (5) = 100
+
+Sources: None other than Phaser Documentation and Lecture
+*/
+
+'use strict';
 let config = {
     type: Phaser.AUTO,
     width: 640,
